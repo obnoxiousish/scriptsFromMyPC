77b81afa6d79592da3e126b9249ec60f4a9126cc
obnoxious <obnoxious@is.too.gay>
2021-09-24 02:25:04 -0700

    mining monitor file

---
+++ monitor.py
@@ -0,0 +1,15 @@
+import psutil
+import subprocess
+
+class main:
+	def __init__(self):
+		self.state = False
+		self.log('Initialized.')
+
+	def startMining(self):
+
+
+	def log(self, msg):
+		print(f'[+] {msg}')
+if __name__ == "__main__":
+	main()
